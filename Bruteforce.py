41444d1629b5c45c2a33547a22a8510c2e2194a9
mtsteo <mateusteotonho@gmail.com>
2024-02-17 18:15:34 -0300

    Nova versao

--- Bruteforce.py
+++ Bruteforce.py
@@ -5,16 +5,18 @@ import hashlib
 sha256hash = input("[+] Enter sha256 Hash value: ")
 
 try:
-    password_list = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt').read(), 'utf-8')
-    for password in password_list.split('\n'):
-        guess = hashlib.sha256(bytes(password, 'utf-8')).hexdigest()
-        if guess == sha256hash:
-            print("[+] The password is: " + str(password))
-            break
-        elif guess != sha256hash:
-            print(f"{guess}")
-            continue
-    else:
-        print("The password does not matched in the list…")
+     with open('10-million-password-list-top-1000000.txt', 'r', encoding='utf-8') as file:
+         
+        password_list = file.read().splitlines()
+        for password in password_list:
+            guess = hashlib.sha256(bytes(password, 'utf-8')).hexdigest()
+            if guess == sha256hash:
+                print("[+] The password is: " + str(password))
+                break
+            elif guess != sha256hash:
+                print(f"{guess}")
+                continue
+            else:
+                print("The password does not matched in the list…")
 except Exception as exc:
     print('There was a problem: %s' % (exc))
